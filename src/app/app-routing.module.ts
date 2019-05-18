c4fee3dbc2507568628ad3aebb7dac5011bc2a45
Tommy Zumstien <zumstein@seattleu.edu>
2019-05-18 12:41:57 -0700

    added search

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { HeaderComponent } from './header/header.component';
 import { GridComponent } from './body/content/grid/grid.component';
+import { SearchComponent } from './body/search/search.component';
 
 const routes: Routes = [
   { path: '', component: HeaderComponent },
+  { path: '', component: SearchComponent },
   { path: 'main', component: MainComponent },
   { path: 'me', component: GridComponent },
 ];
