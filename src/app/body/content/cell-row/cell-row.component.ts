e43baa426510a48f406ca4d4af3469c487ba3b50
Scott <littles1@seattleu.edu>
2019-06-04 18:31:25 -0700

    added row counter

--- src/app/body/content/cell-row/cell-row.component.ts
+++ src/app/body/content/cell-row/cell-row.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { forEach, pick, map } from 'lodash';
 import { CellRowService } from './cell-row.service';
+import { findIndex } from 'lodash/findIndex';
 
 @Component({
   selector: 'app-cell-row',
@@ -9,6 +10,8 @@ import { CellRowService } from './cell-row.service';
 })
 export class CellRowComponent implements OnInit {
   @Input() data: Array<Object>;
+  expandComment: boolean = false;
+  counter: number = 0;
 
   constructor(private getComments: CellRowService) { }
 
@@ -21,14 +24,28 @@ export class CellRowComponent implements OnInit {
         forEach(this.data, (data, index) => {
           if (data.id === id) {
 
-            console.log(resultsFromReddit.comments)
-
             this.data[index]['comments'] = map(resultsFromReddit.comments, result => {
               return pick(result.data, ['author', 'body', 'id', 'name', 'parent_id', 'permalink', 'replies', 'subreddit', 'subreddit_id']);
             });
+
           }
         });
+
         return this.data;
+
       });
+  }
+
+  counterInc() {
+    return this.counter++;
+  }
+
+  expand() {
+    this.expandComment = !this.expandComment;
+  }
+
+  // TODO: implement this function
+  makeComment(id) {
+    // ...
   }
 }
